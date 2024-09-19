ba7eafb0e8355b7c4b149839495b4b7b6588e473
EverestEgg <142887211+EverestEgg@users.noreply.github.com>
2024-09-19 15:27:38 +1000

    finishjs

--- a2/script.js
+++ a2/script.js
@@ -5,8 +5,9 @@ const progressBar = document.querySelector("#progress-bar-fill");
 
 const bodyE = document.querySelector("body");
 const fullScrBtn = document.querySelector("#full-btn");
+const muteunImg = document.querySelector("#mute-img");
+const repImg = document.querySelector("#repeat-img");
 let fullScr = false;
-
 
 video.removeAttribute("controls");
 // playPauseBtn.addEventListener("click", togglePlayPause);
@@ -30,29 +31,59 @@ function updateProgressBar() {
 }
 
 // Toggle between full sceen and standed screen size
-function toggleFull(){
-    if (fullScr == false){
-        video.className = "fulVid";
-        fullScrBtn.className = "ful";
-        bodyE.className = "bodyf";
-        playPauseBtn.className = "ppful"; 
-        // Get play and paus button class working
-        fullScr = true;
-    } else {
-        video.className = "vid";
-        fullScrBtn.className = "";
-        bodyE.className = "";
-        playPauseBtn.className = "";
-        fullScr = false;
-    }
+function toggleFull() {
+  if (fullScr == false) {
+    video.className = "fulVid";
+    fullScrBtn.className = "ful";
+    bodyE.className = "bodyf";
+    playPauseBtn.className = "ppful";
+    // Get play and paus button class working
+    fullScr = true;
+  } else {
+    video.className = "vid";
+    fullScrBtn.className = "";
+    bodyE.className = "";
+    playPauseBtn.className = "";
+    fullScr = false;
+  }
 }
 // Toggles fulll screen on escape key
-document.body.addEventListener('keydown', function(e) {
-    if (e.key == "Escape") {
-      toggleFull();
-    }
-  });
-
-  function toggleMute(){
-    // Turn off sound change icone, on seoncd click turn on sound
+document.body.addEventListener("keydown", function (e) {
+  if (e.key == "Escape") {
+    toggleFull();
   }
+});
+
+// Adds function to the mute button with a changing image to indcate weter it is muted or not
+function toggleMute() {
+  if (video.muted == false) {
+    video.muted = true;
+    muteunImg.src =
+      "https://img.icons8.com/?size=100&id=91635&format=png&color=000000";
+  } else {
+    video.muted = false;
+    muteunImg.src =
+      "https://img.icons8.com/?size=100&id=reqgj3e1uKBy&format=png&color=000000";
+  }
+}
+
+// used to navigate to certain parts of the video that id defined in the timestamps
+function timeJump(x) {
+  video.currentTime = x;
+}
+
+// toggle between the video looping and not with a image change to indicate this
+function toggleRep() {
+  if (video.loop == false) {
+    video.loop = true;
+    repImg.src =
+      "https://img.icons8.com/?size=100&id=91481&format=png&color=000000";
+  } else {
+    video.loop = false;
+    repImg.src = "norep.png";
+  }
+}
+
+function skip5(y) {
+  video.currentTime += y;
+}
